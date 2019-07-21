a44876e4191dcf514bc741e4475805bffc399f6f
maelladegras <mdegras@myrezapp.com>
2019-07-21 16:47:06 +0200

    Added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'pages#about'
+
+  get 'about',   to: 'pages#about',   as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'cgu',     to: 'pages#cgu',     as: :cgu
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
